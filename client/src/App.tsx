50b4473c39d8de0359197cdbb00ae3af5808a3d1
samaksh mittal <sam.mittal44@gmail.com>
2023-03-17 12:42:22 +0530

    chore: declutter react-boilerplate and wordList

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState();
+  return <h1>{"Hii"}</h1>;
+}
+
+export default App;
