e704edcbe35eb1ed633d3dfd51e01e769bf9f46e
samaksh mittal <sam.mittal44@gmail.com>
2023-03-17 20:07:24 +0530

    feat: add guessedletters and words drawing

--- client/src/components/HangmanWord.tsx
+++ client/src/components/HangmanWord.tsx
@@ -1,3 +1,30 @@
 export function HangmanWord() {
-  return <></>;
+  const word = "test";
+  const guessedLetters = ["e"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25rem",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "capitalize",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 }
